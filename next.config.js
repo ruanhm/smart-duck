1c4b4419729200f5b22e140611fa33f0c3e6cc1b
ruanhm <749507784@qq.com>
2018-05-03 16:27:58 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -10,5 +10,8 @@
 
 }*/
 
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+const withCSS = require('@zeit/next-css');
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = (file) => {}
+}
+module.exports = withCSS();
