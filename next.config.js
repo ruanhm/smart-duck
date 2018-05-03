54145a9bbb33649baa1208942e0164ff3c7483d0
ruanhm <749507784@qq.com>
2018-05-03 10:34:16 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-module.exports = {
-  webpack: (config, { buildId, dev }) => {
-    config.plugins = config.plugins.filter(p =>
-      p.constructor.name !== 'UglifyJsPlugin'
-    )
-
-    if (!dev) {
-      const Uglify = require('uglifyjs-webpack-plugin')
-      config.plugins.push(
-        new Uglify({
-          parallel: true,
-          sourceMap: true
-        })
-      )
-    }
+/*module.exports = {
+  webpack(config, { buildId, dev,isServer })  {
+    
     const IgnorePlugin=require('webpack').IgnorePlugin
     config.plugins.push(
      new IgnorePlugin(/tedious|redis$/)
@@ -20,4 +8,7 @@ module.exports = {
     return config
   },
 
-}
+}*/
+
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS()
